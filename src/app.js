93e31b92d7467be424493c1c67917ccad40cb352
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-04-10 21:01:31 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,18 +12,23 @@ app.use(express.json());
 
 // PATCH endpoint for updating user details
 app.patch("/api/v1/details/:id", (req, res) => {
-  const { id } = req.params;
-  const user = userDetails.find((user) => user.id === Number(id));
+  const id = req.params.id;
+  const { name, mail, number } = req.body;
+  
+  // Check if user with given ID exists
+  const user = userDetails.find((user) => user.id == id);
   if (!user) {
     return res.status(404).json({
       status: "failed",
-      message: "User not found",
+      message: "User not found!",
     });
   }
-  const { name, mail, number } = req.body;
+  
+  // Update the user details
   if (name) user.name = name;
   if (mail) user.mail = mail;
   if (number) user.number = number;
+  
   fs.writeFile(
     `${__dirname}/data/userDetails.json`,
     JSON.stringify(userDetails),
@@ -31,11 +36,11 @@ app.patch("/api/v1/details/:id", (req, res) => {
       if (err) {
         return res.status(500).json({
           status: "failed",
-          message: "Error updating user details",
+          message: "Failed to update user details!",
         });
       }
       res.status(200).json({
-        status: "success",
+        status: "Success",
         message: "User details updated successfully",
         data: {
           userDetails: user,
@@ -55,8 +60,14 @@ app.post("/api/v1/details", (req, res) => {
     `${__dirname}/data/userDetails.json`,
     JSON.stringify(userDetails),
     (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: "failed",
+          message: "Failed to register new user!",
+        });
+      }
       res.status(201).json({
-        status: "success",
+        status: "Success",
         message: "User registered successfully",
         data: {
           userDetails: newUser,
@@ -69,31 +80,33 @@ app.post("/api/v1/details", (req, res) => {
 // GET endpoint for sending the details of users
 app.get("/api/v1/details", (req, res) => {
   res.status(200).json({
-    status: "success",
-    message: "Details of all users fetched successfully",
+    status: "Success",
+    message: "Detail of users fetched successfully",
     data: {
       userDetails,
     },
   });
 });
 
-// GET endpoint for sending the details of a user by id
-app.get("/api/v1/details/:id", (req, res) => {
-  const { id } = req.params;
-  const user = userDetails.find((user) => user.id === Number(id));
-  if (!user) {
-    return res.status(404).json({
+// GET endpoint for sending the details of users by id
+app.get("/api/v1/userdetails/:id", (req, res) => {
+  let { id } = req.params;
+  id *= 1;
+  const details = userDetails.find((details) => details.id === id);
+  if (!details) {
+    return res.status(404).send({
       status: "failed",
-      message: "User not found",
+      message: "User not found!",
+    });
+  } else {
+    res.status(200).send({
+      status: "success",
+      message: "Details of users fetched successfully",
+      data: {
+        details,
+      },
     });
   }
-  res.status(200).json({
-    status: "success",
-    message: "Details of user fetched successfully",
-    data: {
-      userDetails: user,
-    },
-  });
 });
 
 module.exports = app;
