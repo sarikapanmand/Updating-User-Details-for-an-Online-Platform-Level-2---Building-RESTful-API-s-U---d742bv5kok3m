bbeafe9154d9bcee5d4bd8744fa24f9349045674
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-04-10 21:38:30 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,24 +2,25 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
-// Importing user details from userDetails.json file
+// Importing products from userDetails.json file
 const userDetails = JSON.parse(
   fs.readFileSync(`${__dirname}/data/userDetails.json`)
 );
 
-// Middlewares
+//Middlewares
 app.use(express.json());
 
-// Endpoint for registering a new user
+// POST endpoint for registering new user
 app.post("/api/v1/details", (req, res) => {
   const newId = userDetails[userDetails.length - 1].id + 1;
-  const { name, email, number } = req.body;
-  const newUser = { id: newId, name, email, number };
+  const { name, mail, number } = req.body;
+  const newUser = { id: newId, name, mail, number };
   userDetails.push(newUser);
   fs.writeFile(
     `${__dirname}/data/userDetails.json`,
     JSON.stringify(userDetails),
     (err) => {
+      if (err) throw err;
       res.status(201).json({
         status: "success",
         message: "User registered successfully",
@@ -31,7 +32,7 @@ app.post("/api/v1/details", (req, res) => {
   );
 });
 
-// Endpoint for fetching all user details
+// GET endpoint for sending the details of users
 app.get("/api/v1/details", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -42,58 +43,52 @@ app.get("/api/v1/details", (req, res) => {
   });
 });
 
-// Endpoint for fetching user details by id
+// GET endpoint for sending the details of users by id
 app.get("/api/v1/details/:id", (req, res) => {
   const { id } = req.params;
-  const user = userDetails.find((user) => user.id === parseInt(id));
+  const user = userDetails.find((user) => user.id === Number(id));
   if (!user) {
     return res.status(404).json({
-      status: "fail",
-      message: "User not found",
+      status: "failed",
+      message: "User not found!",
     });
   } else {
     res.status(200).json({
       status: "success",
-      message: "User details fetched successfully",
+      message: "Details of user fetched successfully",
       data: {
-        user,
+        userDetails: user,
       },
     });
   }
 });
 
-// Endpoint for updating user details by id
+// PATCH endpoint for updating user details
 app.patch("/api/v1/details/:id", (req, res) => {
   const { id } = req.params;
-  const { name, email, number } = req.body;
-  const user = userDetails.find((user) => user.id === parseInt(id));
-  if (!user) {
+  const userIndex = userDetails.findIndex((user) => user.id === Number(id));
+  if (userIndex === -1) {
     return res.status(404).json({
-      status: "fail",
-      message: "User not found",
+      status: "failed",
+      message: "User not found!",
     });
   } else {
-    user.name = name || user.name;
-    user.email = email || user.email;
-    user.number = number || user.number;
+    const { name, mail, number } = req.body;
+    userDetails[userIndex].name = name || userDetails[userIndex].name;
+    userDetails[userIndex].mail = mail || userDetails[userIndex].mail;
+    userDetails[userIndex].number = number || userDetails[userIndex].number;
     fs.writeFile(
       `${__dirname}/data/userDetails.json`,
       JSON.stringify(userDetails),
       (err) => {
-        if (err) {
-          return res.status(500).json({
-            status: "error",
-            message: "Error updating user details",
-          });
-        } else {
-          return res.status(200).json({
-            status: "success",
-            message: `User details updated successfully for id: ${id}`,
-            data: {
-              user,
-            },
-          });
-        }
+        if (err) throw err;
+        res.status(200).json({
+          status: "success",
+          message: "User details updated successfully",
+          data: {
+            userDetails: userDetails[userIndex],
+          },
+        });
       }
     );
   }
