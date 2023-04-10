f3238dcf408bdd06d2217b39a0da3d405e392058
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-04-10 20:53:55 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,40 @@ const userDetails = JSON.parse(
 //Middlewares
 app.use(express.json());
 
-// Write PATCH endpoint for editing user details
+// PATCH endpoint for updating user details
+app.patch("/api/v1/details/:id", (req, res) => {
+  const { id } = req.params;
+  const user = userDetails.find((user) => user.id === Number(id));
+  if (!user) {
+    return res.status(404).json({
+      status: "failed",
+      message: "User not found",
+    });
+  }
+  const { name, mail, number } = req.body;
+  if (name) user.name = name;
+  if (mail) user.mail = mail;
+  if (number) user.number = number;
+  fs.writeFile(
+    `${__dirname}/data/userDetails.json`,
+    JSON.stringify(userDetails),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: "failed",
+          message: "Error updating user details",
+        });
+      }
+      res.status(200).json({
+        status: "success",
+        message: "User details updated successfully",
+        data: {
+          userDetails: user,
+        },
+      });
+    }
+  );
+});
 
 // POST endpoint for registering new user
 app.post("/api/v1/details", (req, res) => {
@@ -23,7 +56,7 @@ app.post("/api/v1/details", (req, res) => {
     JSON.stringify(userDetails),
     (err) => {
       res.status(201).json({
-        status: "Success",
+        status: "success",
         message: "User registered successfully",
         data: {
           userDetails: newUser,
@@ -36,33 +69,31 @@ app.post("/api/v1/details", (req, res) => {
 // GET endpoint for sending the details of users
 app.get("/api/v1/details", (req, res) => {
   res.status(200).json({
-    status: "Success",
-    message: "Detail of users fetched successfully",
+    status: "success",
+    message: "Details of all users fetched successfully",
     data: {
       userDetails,
     },
   });
 });
 
-// GET endpoint for sending the details of users by id
-app.get("/api/v1/userdetails/:id", (req, res) => {
-  let { id } = req.params;
-  id *= 1;
-  const details = userDetails.find((details) => details.id === id);
-  if (!details) {
-    return res.status(404).send({
+// GET endpoint for sending the details of a user by id
+app.get("/api/v1/details/:id", (req, res) => {
+  const { id } = req.params;
+  const user = userDetails.find((user) => user.id === Number(id));
+  if (!user) {
+    return res.status(404).json({
       status: "failed",
-      message: "User not found!",
-    });
-  } else {
-    res.status(200).send({
-      status: "success",
-      message: "Details of users fetched successfully",
-      data: {
-        details,
-      },
+      message: "User not found",
     });
   }
+  res.status(200).json({
+    status: "success",
+    message: "Details of user fetched successfully",
+    data: {
+      userDetails: user,
+    },
+  });
 });
 
 module.exports = app;
