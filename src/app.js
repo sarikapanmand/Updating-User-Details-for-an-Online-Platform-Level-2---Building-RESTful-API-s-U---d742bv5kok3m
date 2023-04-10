dd59a1061deb1640554f32acbac0a52097977489
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-04-10 21:17:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,41 +10,44 @@ const userDetails = JSON.parse(
 //Middlewares
 app.use(express.json());
 
-// PATCH endpoint for editing user details
+// Write PATCH endpoint for editing user details
 app.patch("/api/v1/details/:id", (req, res) => {
-  const { id } = req.params;
-  const { name, mail, number } = req.body;
-  const index = userDetails.findIndex((user) => user.id === Number(id));
-  
-  if (index === -1) {
+  const id = parseInt(req.params.id);
+  const user = userDetails.find((user) => user.id === id);
+
+  if (!user) {
     return res.status(404).json({
       status: "failed",
-      message: "User not found",
+      message: "User not found!",
     });
-  }
-  
-  const updatedUser = { ...userDetails[index], name, mail, number };
-  userDetails[index] = updatedUser;
-  
-  fs.writeFile(
-    `${__dirname}/data/userDetails.json`,
-    JSON.stringify(userDetails),
-    (err) => {
-      if (err) {
-        return res.status(500).json({
-          status: "failed",
-          message: "Error updating user details",
-        });
+  } else {
+    const { name, mail, number } = req.body;
+    if (name) user.name = name;
+    if (mail) user.mail = mail;
+    if (number) user.number = number;
+
+    fs.writeFile(
+      `${__dirname}/data/userDetails.json`,
+      JSON.stringify(userDetails),
+      (err) => {
+        if (err) {
+          return res.status(500).json({
+            status: "failed",
+            message: "Failed to update user details",
+            error: err.message,
+          });
+        } else {
+          return res.status(200).json({
+            status: "success",
+            message: "User details updated successfully",
+            data: {
+              userDetails: user,
+            },
+          });
+        }
       }
-      return res.status(200).json({
-        status: "success",
-        message: "User details updated successfully",
-        data: {
-          userDetails: updatedUser,
-        },
-      });
-    }
-  );
+    );
+  }
 });
 
 // POST endpoint for registering new user
@@ -60,16 +63,18 @@ app.post("/api/v1/details", (req, res) => {
       if (err) {
         return res.status(500).json({
           status: "failed",
-          message: "Error registering user",
+          message: "Failed to register user",
+          error: err.message,
+        });
+      } else {
+        return res.status(201).json({
+          status: "success",
+          message: "User registered successfully",
+          data: {
+            userDetails: newUser,
+          },
         });
       }
-      return res.status(201).json({
-        status: "success",
-        message: "User registered successfully",
-        data: {
-          userDetails: newUser,
-        },
-      });
     }
   );
 });
@@ -78,7 +83,7 @@ app.post("/api/v1/details", (req, res) => {
 app.get("/api/v1/details", (req, res) => {
   res.status(200).json({
     status: "success",
-    message: "Details of all users fetched successfully",
+    message: "Details of users fetched successfully",
     data: {
       userDetails,
     },
@@ -87,21 +92,22 @@ app.get("/api/v1/details", (req, res) => {
 
 // GET endpoint for sending the details of users by id
 app.get("/api/v1/details/:id", (req, res) => {
-  const { id } = req.params;
-  const user = userDetails.find((user) => user.id === Number(id));
+  const id = parseInt(req.params.id);
+  const user = userDetails.find((user) => user.id === id);
   if (!user) {
     return res.status(404).json({
       status: "failed",
-      message: "User not found",
+      message: "User not found!",
+    });
+  } else {
+    return res.status(200).json({
+      status: "success",
+      message: "Details of user fetched successfully",
+      data: {
+        userDetails: user,
+      },
     });
   }
-  return res.status(200).json({
-    status: "success",
-    message: "Details of user fetched successfully",
-    data: {
-      userDetails: user,
-    },
-  });
 });
 
 module.exports = app;
