bd85efac536ccc13cf00de06823847985da439d1
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-05-09 05:38:41 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,50 @@ const userDetails = JSON.parse(
 //Middlewares
 app.use(express.json());
 
+// Write PATCH endpoint for editing user details
+// PATCH endpoint for updating user details
+app.patch("/api/v1/details/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, mail, number } = req.body;
+  const userIndex = userDetails.findIndex(user => user.id == id);
+
+  if (userIndex === -1) {
+    return res.status(404).json({
+      status: "failed",
+      message: "User not found!",
+    });
+  }
+
+  const updatedUser = {
+    id: userDetails[userIndex].id,
+    name: name || userDetails[userIndex].name,
+    mail: mail || userDetails[userIndex].mail,
+    number: number || userDetails[userIndex].number,
+  };
+
+  userDetails[userIndex] = updatedUser;
+
+  fs.writeFile(
+    `${__dirname}/data/userDetails.json`,
+    JSON.stringify(userDetails),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: "failed",
+          message: "Server error",
+        });
+      }
+
+      res.status(200).json({
+        status: "success",
+        message: `User details updated successfully for id: ${id}`,
+        user: updatedUser,
+      });
+    }
+  );
+});
+
+
 // POST endpoint for registering new user
 app.post("/api/v1/details", (req, res) => {
   const newId = userDetails[userDetails.length - 1].id + 1;
@@ -20,9 +64,8 @@ app.post("/api/v1/details", (req, res) => {
     `${__dirname}/data/userDetails.json`,
     JSON.stringify(userDetails),
     (err) => {
-      if (err) throw err;
       res.status(201).json({
-        status: "success",
+        status: "Success",
         message: "User registered successfully",
         data: {
           userDetails: newUser,
@@ -35,8 +78,8 @@ app.post("/api/v1/details", (req, res) => {
 // GET endpoint for sending the details of users
 app.get("/api/v1/details", (req, res) => {
   res.status(200).json({
-    status: "success",
-    message: "Details of all users fetched successfully",
+    status: "Success",
+    message: "Detail of users fetched successfully",
     data: {
       userDetails,
     },
@@ -44,53 +87,23 @@ app.get("/api/v1/details", (req, res) => {
 });
 
 // GET endpoint for sending the details of users by id
-app.get("/api/v1/details/:id", (req, res) => {
-  const { id } = req.params;
-  const user = userDetails.find((user) => user.id === Number(id));
-  if (!user) {
-    return res.status(404).json({
+app.get("/api/v1/userdetails/:id", (req, res) => {
+  let { id } = req.params;
+  id *= 1;
+  const details = userDetails.find((details) => details.id === id);
+  if (!details) {
+    return res.status(404).send({
       status: "failed",
       message: "User not found!",
     });
   } else {
-    res.status(200).json({
+    res.status(200).send({
       status: "success",
-      message: "Details of user fetched successfully",
+      message: "Details of users fetched successfully",
       data: {
-        userDetails: user,
+        details,
       },
     });
-  }
-});
-
-// PATCH endpoint for updating user details
-app.patch("/api/v1/details/:id", (req, res) => {
-  const { id } = req.params;
-  const userIndex = userDetails.findIndex((user) => user.id === Number(id));
-  if (userIndex === -1) {
-    return res.status(404).json({
-      status: "failed",
-      message: "User not found!",
-    });
-  } else {
-    const { name, mail, number } = req.body;
-    userDetails[userIndex].name = name || userDetails[userIndex].name;
-    userDetails[userIndex].mail = mail || userDetails[userIndex].mail;
-    userDetails[userIndex].number = number || userDetails[userIndex].number;
-    fs.writeFile(
-      `${__dirname}/data/userDetails.json`,
-      JSON.stringify(userDetails),
-      (err) => {
-        if (err) throw err;
-        res.status(200).json({
-          status: "success",
-          message: "User details updated successfully",
-          data: {
-            userDetails: userDetails[userIndex],
-          },
-        });
-      }
-    );
   }
 });
 
